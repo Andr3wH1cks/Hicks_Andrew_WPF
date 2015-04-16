b26cf6da0fe7bd8bb7eb42010aaa64cd9b7d531a
Andr3wH1cks <Andrew@Andrews-MacBook-Pro.local>
2015-04-16 08:20:42 -0400

    Math Equations
    Creating the equations for the variables

--- Expressions_Assignment/js/script.js
+++ Expressions_Assignment/js/script.js
@@ -5,10 +5,21 @@ Dan Williams
  */
 
 
-// Here I am creating variables and prompts
+// Here I am creating variables and prompts.
 
-alert("You're driving to your getaway.");
+alert("You're driving to two places today.");
 
-var distance = prompt("How many miles are you driving?");
-var time = prompt("How long does it take you to get there?");
-var gas = prompt("How many gallons of gas are you using to get there?");
+var distanceToA = /*Assigning the prompts to read as numbers*/Number(prompt("How many miles are you driving to point A?"));
+var distanceToB = /*Assigning the prompts to read as numbers*/Number(prompt("How many miles are you driving to point B?"));
+var time = /*Assigning the prompts to read as numbers*/Number(prompt("How many hours does it take you to get there?"));
+var gas = /*Assigning the prompts to read as numbers*/Number(prompt("How many gallons of gas are you using to get there?"));
+
+// Here are the variables for the math equations.
+
+var speed = (distanceToA + distanceToB)/time;
+var mileage = (distanceToA + distanceToB)/gas;
+distanceToA += distanceToB;
+
+// Printing variables to the console.
+console.log ("You're total distance traveled will me " + String(distanceToA) + " miles.");
+console
