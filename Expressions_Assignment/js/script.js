9380f3a87f98daba0fc603b3b51f502461ba1c9e
Andr3wH1cks <Andrew@Andrews-MacBook-Pro.local>
2015-04-16 07:29:34 -0400

    Variables
    Committing variable prompts

--- Expressions_Assignment/js/script.js
+++ Expressions_Assignment/js/script.js
@@ -1,3 +1,14 @@
-/**
- * Created by Andrew on 4/16/15.
+/* Andrew Hicks
+SDI Term 1
+3/16/15
+Dan Williams
  */
+
+
+// Here I am creating variables and prompts
+
+alert("You're driving to your getaway.");
+
+var distance = prompt("How many miles are you driving?");
+var time = prompt("How long does it take you to get there?");
+var gas = prompt("How many gallons of gas are you using to get there?");
