d57e4ff54d9c5a48ba31110e873538ee7d5f2966
Andr3wH1cks <Andrew@Andrews-MacBook-Pro.local>
2015-04-16 08:29:11 -0400

    Spellcheck/variables
    Grammar fixes and adding new variables

--- Expressions_Assignment/js/script.js
+++ Expressions_Assignment/js/script.js
@@ -11,16 +11,19 @@ alert("You're driving to two places today.");
 
 var distanceToA = /*Assigning the prompts to read as numbers*/Number(prompt("How many miles are you driving to point A?"));
 var distanceToB = /*Assigning the prompts to read as numbers*/Number(prompt("How many miles are you driving to point B?"));
-var time = /*Assigning the prompts to read as numbers*/Number(prompt("How many hours does it take you to get there?"));
+var timeToA = /*Assigning the prompts to read as numbers*/Number(prompt("How many hours are you driving to point A?"));
+var timeToB = /*Assigning the prompts to read as numbers*/Number(prompt("How many hours are you driving to point B?"));
 var gas = /*Assigning the prompts to read as numbers*/Number(prompt("How many gallons of gas are you using to get there?"));
 
 // Here are the variables for the math equations.
 
-var speed = (distanceToA + distanceToB)/time;
+var speed = (distanceToA + distanceToB)/(timeToA + timeToB);
 var mileage = (distanceToA + distanceToB)/gas;
 distanceToA += distanceToB;
+timeToA += timeToB;
 
 // Printing variables to the console.
-console.log ("Your total distance traveled will me " + String(distanceToA) + " miles.");
-console.log ("Your average speed fr your trip was " + String(speed) + " miles per hour.");
+console.log ("Your total distance traveled was " + String(distanceToA) + " miles.");
+console.log ("Your total time on the road was " + String(timeToA) + " hours.");
+console.log ("Your average speed for your trip was " + String(speed) + " miles per hour.");
 console.log ("Your average fuel mileage was " + String(mileage) + " miles per gallon.");
