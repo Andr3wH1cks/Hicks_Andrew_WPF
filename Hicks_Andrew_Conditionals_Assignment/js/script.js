c5ae3d7987192f0af8e6d21e594b7d5da842a17e
Andr3wH1cks <Andrew@Andrews-MacBook-Pro.local>
2015-04-23 08:40:58 -0400

    Completing conditionals
    Completing conditionals

--- Hicks_Andrew_Conditionals_Assignment/js/script.js
+++ Hicks_Andrew_Conditionals_Assignment/js/script.js
@@ -10,8 +10,8 @@ Dan Williams
 alert("Based on where you live, can you have a dog?");
 
 // Prompt variables
-
-var whereDoYouLive = Number(prompt("Do you rent or own? \nType 1 for rent and 2 for own."));
+var moving = Number(prompt("Are you moving? \nType 1 for yes, 2 for no."));
+var whereWillYouLive = Number(prompt("Will you or do you now rent or own? \nType 1 for rent, 2 for own."));
 var weightRestriction = Number(prompt("In pounds, what is the weight restriction for the dog at the place you rent?"));
 var weightOfDog = Number(prompt("In pounds, how much does your dog weigh?"));
 
@@ -20,20 +20,39 @@ var weightOfDog = Number(prompt("In pounds, how much does your dog weigh?"));
 
 var overWeight = weightOfDog - weightRestriction;
 var underWeight = weightRestriction - weightOfDog;
+var weightMath;
+
+if (moving == 1) {
 
 
-if (whereDoYouLive == 2){
-    console.log ("You own your house, you can do whatever you want!");
-
-}
-else if (whereDoYouLive == 1) {
-    if (weightOfDog <= weightRestriction) {
-
-        console.log("Your dog is " + String(underWeight) + " pound(s) under the restriction. You're good to go!");
+    if (whereWillYouLive == 2) {
+        console.log("You own your house, you can do whatever you want!");
+        console.log("See what the difference is if you have to rent.");
 
     }
+    else {
+        if (whereWillYouLive == 1) {
+            weightMath = (weightOfDog <= weightRestriction) ? "Your dog is " + String(underWeight) + " pound(s) under the restriction. You're good to go!" : "Your dog is " + String(overWeight) + " pound(s) of the restriction. YOu either need to put your dog on a diet, or find another place to live.";
+            console.log(weightMath);
+        }
+    }
 }
-else {
-    console.log ("Your dog is " + String(overWeight) + " pound(s) of the restriction. YOu either need to put your dog on a diet, or find another place to live.");
 
+
+        else if (moving == 2) {
+            console.log("So you're not moving, but you still want a dog.");
+            if (whereWillYouLive == 2) {
+                console.log("You own your house, you can do whatever you want!");
+                console.log("See what the difference is if you have to rent.");
+
+            }
+            else {
+                if (whereWillYouLive == 1) {
+
+                    weightMath = (weightOfDog <= weightRestriction) ?
+                    "Your dog is " + String(underWeight) + " pound(s) under the restriction. You're good to go!" :
+                    "Your dog is " + String(overWeight) + " pound(s) of the restriction. YOu either need to put your dog on a diet, or find another place to live.";
+                    console.log(weightMath);
+                }
+            }
 }
