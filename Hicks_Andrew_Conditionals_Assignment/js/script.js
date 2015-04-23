b3e29980f466a0f064ecfe3b1b47d2fb9cda96c6
Andr3wH1cks <Andrew@Andrews-MacBook-Pro.local>
2015-04-23 08:13:33 -0400

    Variables and conditionals
    Variables and conditionals

--- Hicks_Andrew_Conditionals_Assignment/js/script.js
+++ Hicks_Andrew_Conditionals_Assignment/js/script.js
@@ -12,5 +12,28 @@ alert("Based on where you live, can you have a dog?");
 // Prompt variables
 
 var whereDoYouLive = Number(prompt("Do you rent or own? \nType 1 for rent and 2 for own."));
-var whatIsRestriction = Number(prompt("What is the weight restriction for the dog at the place you rent?"));
-var weightOfDog = Number(prompt("How much does your dog weigh?"));
+var weightRestriction = Number(prompt("In pounds, what is the weight restriction for the dog at the place you rent?"));
+var weightOfDog = Number(prompt("In pounds, how much does your dog weigh?"));
+
+
+// Math Variables
+
+var overWeight = weightOfDog - weightRestriction;
+var underWeight = weightRestriction - weightOfDog;
+
+
+if (whereDoYouLive == 2){
+    console.log ("You own your house, you can do whatever you want!");
+
+}
+else if (whereDoYouLive == 1) {
+    if (weightOfDog <= weightRestriction) {
+
+        console.log("Your dog is " + String(underWeight) + " pound(s) under the restriction. You're good to go!");
+
+    }
+}
+else {
+    console.log ("Your dog is " + String(overWeight) + " pound(s) of the restriction. YOu either need to put your dog on a diet, or find another place to live.");
+
+}
