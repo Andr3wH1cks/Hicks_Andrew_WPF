984a46b6b5c2154c200cb9db908ec354fb1a9669
Andr3wH1cks <Andrew@Andrews-MacBook-Pro.local>
2015-04-28 22:24:37 -0400

    Function/formula
    getting the formula for interest

--- Hicks_Andrew_Functions_Assignment/js/scrpit.js
+++ Hicks_Andrew_Functions_Assignment/js/scrpit.js
@@ -5,10 +5,25 @@ Dan Williams
  */
 
 
-//variables
-var interestRateVariables = calcInterest(Number(prompt("Principal")), Number(prompt("Rate")), Number(prompt("Time")));
 
-function calcInterest(P, r, t){
-    var interest = P * r * t;
+
+//variables
+var interestRateVariables = calcInterest (p, r, t);
+var p = Number(prompt("How much is in your savings account"));
+var rate = Number(prompt("What is your interest rate?"));
+var t = Number(prompt("In years, how long will you be keeping your money in your account without adding or removing anything?"));
+var r = rate/100;
+var interestFormula = calcInterest(p, r, t);
+
+
+// Functions
+
+function calcInterest(p, r, t){
+    var interest = p * r * t;
     console.log (interest);
+
+    return interest;
+
 }
+
+ interestRateVariables = calcInterest(interestFormula);
